f6762ed7e62ef62efb6381b3cd9e8a677491e8f7
Can Ivit <canivit@users.noreply.github.com>
2023-10-08 18:22:30 -0400

    complete lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./Nav";
+import Kanbas from "./Kanbas";
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
+import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div>
+        <Nav/>
+        <Routes>
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/kanbas" element={<Kanbas />} />
+          <Route path="/hello" element={<HelloWorld />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/Nav.tsx
@@ -0,0 +1,26 @@
+import path from "path";
+import { Link, useLocation } from "react-router-dom";
+
+function Nav() {
+  const { pathname } = useLocation();
+  const activeClass = getActiveClass(pathname);
+  return (
+    <nav className="nav nav-tabs mt-2">
+      <Link className={`nav-link ${activeClass("a3")}`} to="/Labs/a3">
+        A3
+      </Link>
+      <Link className={`nav-link ${activeClass("hello")}`} to="/hello">
+        Hello
+      </Link>
+      <Link className={`nav-link ${activeClass("Kanbas")}`} to="/Kanbas">
+        Kanbas
+      </Link>
+    </nav>
+  );
+}
+
+function getActiveClass(pathname: string) {
+  return (check: string) => (pathname.includes(check) ? "active" : "");
+}
+
+export default Nav;
