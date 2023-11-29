63933c6e8e2bbd60cbf4cf2294bbb1afc46f89a4
Can Ivit <canivit@users.noreply.github.com>
2023-11-29 13:28:24 -0500

    support multiple sessions

--- src/a6/users/client.ts
+++ src/a6/users/client.ts
@@ -3,46 +3,50 @@ import { API_BASE } from "../../Kanbas/client/course";
 
 const USERS_API = `${API_BASE}/users`;
 
+const request = axios.create({
+  withCredentials: true,
+});
+
 export async function signin(credentials: Credentials): Promise<User> {
-  const response = await axios.post(`${USERS_API}/signin`, credentials);
+  const response = await request.post(`${USERS_API}/signin`, credentials);
   return response.data;
 }
 
 export async function getAccount(): Promise<User> {
-  const response = await axios.get(`${USERS_API}/account`);
+  const response = await request.get(`${USERS_API}/account`);
   return response.data;
 }
 
 export async function updateUser(user: User): Promise<void> {
-  await axios.post(`${USERS_API}/${user._id}`, user);
+  await request.post(`${USERS_API}/${user._id}`, user);
 }
 
 export async function findAllUsers(): Promise<User[]> {
-  const response = await axios.get(`${USERS_API}`);
+  const response = await request.get(`${USERS_API}`);
   return response.data;
 }
 
 export async function createUser(user: User): Promise<User> {
-  const response = await axios.put(`${USERS_API}`, user);
+  const response = await request.put(`${USERS_API}`, user);
   return response.data;
 }
 
 export async function findUserById(id: string) {
-  const response = await axios.get(`${USERS_API}/${id}`);
+  const response = await request.get(`${USERS_API}/${id}`);
   return response.data;
 }
 
 export async function deleteUser(id: string): Promise<void> {
-  await axios.delete(`${USERS_API}/${id}`);
+  await request.delete(`${USERS_API}/${id}`);
 }
 
 export async function signup(credentials: Credentials): Promise<User> {
-  const response = await axios.post(`${USERS_API}/signup`, credentials);
+  const response = await request.post(`${USERS_API}/signup`, credentials);
   return response.data;
 }
 
 export async function signout(): Promise<void> {
-  await axios.get(`${USERS_API}/signout`);
+  await request.get(`${USERS_API}/signout`);
 }
 
 export type Credentials = {
